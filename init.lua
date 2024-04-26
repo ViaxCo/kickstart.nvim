31da1832c91cb9508e065048438b2945b5c772e0
Victor Ajibade <ajibadevic@gmail.com>
2024-04-26 16:10:33 +0100

    feat: add transparency to theme

--- init.lua
+++ init.lua
@@ -782,6 +782,9 @@ require('lazy').setup({
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     'folke/tokyonight.nvim',
+    opts = {
+      transparent = true,
+    },
     -- 'catppuccin/nvim',
     -- name = 'catppuccin',
     priority = 1000, -- Make sure to load this before all the other start plugins.
